40105e5a12fea8aa67e9436778b3d132d40caccf
Thomas <thomasgeboers@gmail.com>
2016-12-23 22:24:56 +0100

    fmt

--- assignment/src/e_fullscreen_windows.rs
+++ assignment/src/e_fullscreen_windows.rs
@@ -27,10 +27,5 @@
 // Add imports here
 
 
-/// **TODO**: You are free to choose the name for your window manager. As we
-/// will use automated tests when grading your assignment, indicate here the
-/// name of your window manager data type so we can just use `WMName` instead
-/// of having to manually figure out your window manager name.
-///
 /// Replace `()` with the name of your window manager data type.
 pub type WMName = ();
